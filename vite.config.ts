9ab620e03be4c4899a2aea111400ab860be75899
Efe Kırbaş <efekrbass@gmail.com>
2026-08-09 09:19:02 +0300

    Add tailwindcss vite plugin to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     TanStackRouterVite(),
     react(),
     tsconfigPaths(),
